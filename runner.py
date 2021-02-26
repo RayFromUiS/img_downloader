7510fc403add5d417a806fb2ebb8d7085b28f3c0
ray <chunlei_uis@hotmail.com>
2021-02-26 22:34:45 +0800

    trivial

--- runner.py
+++ runner.py
@@ -17,8 +17,8 @@ if __name__=='__main__':
     uri = 'mysql+pymysql://root:jinzheng1706@139.198.191.224:3308/news_oil'
     engine = db_connect(uri)
     create_table(engine)
-    preview_save_dir = '/Users/root1/mnt/news_img_dir/preview_imgs'
-    content_save_dir = '/Users/root1/mnt/news_img_dir/content_imgs'
+    preview_save_dir = '/mnt/news_img_dir/preview_imgs'
+    content_save_dir = '/mnt/news_img_dir/content_imgs'
     img_table='imgs_location'
     content_urls_con = [] #list for saving all the table procssed content img
     content_urls_not_con = []
@@ -27,22 +27,14 @@ if __name__=='__main__':
     preview_imgs_urls = grap_preview_imgs_urls(img_table, engine) #noprocessed preview urls
     content_imgs_urls = grap_content_imgs_urls(img_table, engine) #nonprocessed content url
     for table in table_name_pro:
-#         ua = UserAgent()
-#         user_agent = ua.ie
         not_down_preview = get_preview_imgs_url(table, engine, preview_imgs_urls)
         preview_imgs_downloaded,imgs_not_downloaded = download_imgs(not_down_preview,
                                                             'preview_img_link','preview_img_local',preview_save_dir,table)
-#         preview_urls_con.append(imgs_downloaded)
-#         preview_urls_not_con.append(imgs_not_downloaded)
-        
 
-#         ua = UserAgent()
-#         user_agent = ua.ie
         not_down_content =  get_content_imgs_url(table, engine, content_imgs_urls)
         content_imgs_downloaded,imgs_not_downloaded = download_imgs(not_down_content,'title_img_url','title_img_local',
                                                            content_save_dir,table)
-#         content_urls_con.append(imgs_downloaded)
-#         content_urls_not_con.append(imgs_not_downloaded)
+
         save_to_db(content_imgs_downloaded,preview_imgs_downloaded,img_table,engine)
         break
         
